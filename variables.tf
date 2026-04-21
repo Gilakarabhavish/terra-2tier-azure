298ab83f541e3be3b2f08504a864d89582680f9a
bob <bhavishgilakara@gmail.com>
2026-04-21 16:41:47 +0530

    Added .gitignore for Terraform

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "location" {
+  default = "korea central"
+}
+
+variable "resource_group_name" {
+  default = "bobrg"
+}
+
+variable "admin_username" {
+  default = "azureadmin"
+}
+
+variable "admin_password" {
+  default = "password@123"
+}
+
+variable "mysql_admin" {
+  default = "bablu"
+}
+
+variable "mysql_password" {
+  default = "StrongPass#123"
+}
+
