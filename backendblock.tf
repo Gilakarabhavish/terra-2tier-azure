1c3793ce2de50d697e70dd4d501cc886d276270c
Gilakarabhavish <bhavishgilakara@gmail.com>
2026-04-22 00:00:32 +0530

    backendblock.tf

--- backendblock.tf
+++ backendblock.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = var.resource_group_name
+    resource_group_name  = "bob-rg"
     storage_account_name = "accountstore321"
     container_name       = "tf-state"
     key                  = "terraform.tfstate"
